3f55a2b3d13664054eb1eb2d52eec7a50d3ba319
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-04-20 08:45:53 -0400

    finished auth

---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="hero-img-frame">
+      <img className="hero-img" src="https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" />
+    </div> 
+    <h1>Where birds can chirp.</h1>
+  </div>
+)
